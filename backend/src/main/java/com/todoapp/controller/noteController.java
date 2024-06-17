4c0072ff507702b5c8f6540737bc67c9f25b48c8
VISHAL <vishalpatel13112000@gmail.com>
2024-06-17 16:52:39 +0530

    VISHAL CONTRIBUTION

--- backend/src/main/java/com/todoapp/controller/noteController.java
+++ backend/src/main/java/com/todoapp/controller/noteController.java
@@ -2,11 +2,14 @@ package com.todoapp.controller;
 
 import com.todoapp.entity.notes;
 import com.todoapp.repository.NoteRepo;
+import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class noteController {
@@ -15,5 +18,9 @@ public class noteController {
     @GetMapping("/notes")
     List<notes> getAllNotes(){
         return noteRepo.findAll();
+    }
+    @GetMapping("/notes/{id}")
+    Optional<notes> findnotebyid(@PathVariable ("id") Long id){
+        return noteRepo.findById(id);
     }
 }
